972fdfad84265bffddda966814f13d85891c29f7
radopeti <rVwcVG9EUXp>
2021-07-01 16:57:26 +0200

    project init

---
+++ src/main/java/com/radopeti/springredisheroku/SpringRedisHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.radopeti.springredisheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radopeti/springredisheroku/SpringRedisHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radopeti.springredisheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
