15773040d9aa2643c4f2366e3def30e2a75eb18f
radopeti <rVwcVG9EUXp>
2021-07-06 17:19:38 +0200

    extend TestController to test cache zone eviction

--- src/main/java/com/radopeti/springredisheroku/web/controller/TestController.java
+++ src/main/java/com/radopeti/springredisheroku/web/controller/TestController.java
@@ -1,20 +1,47 @@
 package com.radopeti.springredisheroku.web.controller;
 
+import com.radopeti.springredisheroku.web.service.ParkingService;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private final ParkingService parkingService;
+    private final CacheManager cacheManager;
+
+    public TestController(ParkingService parkingService, CacheManager cacheManager) {
+        this.parkingService = parkingService;
+        this.cacheManager = cacheManager;
+    }
 
     @GetMapping("/")
     public ResponseEntity<String> getHiOnIndexPage() {
         return ResponseEntity.ok("Hi on the index page!");
     }
 
-    @GetMapping("/hi")
-    public ResponseEntity<String> getHi() {
-        return ResponseEntity.ok("Hi!");
+    @GetMapping("/information")
+    public ResponseEntity<String> getHi(@RequestParam("id") String id) {
+        String information;
+        if (cacheManager.getCache("itemCache").get(id) == null) {
+            information = parkingService.getInformation("PlateNumber", "ABC123");
+            cacheManager.getCache("itemCache").put(id, information);
+        } else {
+            information = (String) cacheManager.getCache("itemCache").get(id).get();
+        }
+        return ResponseEntity.ok(information);
+    }
+
+    @PostMapping("/evict")
+    public ResponseEntity<Void> removeCacheId(@RequestParam("id") String id) {
+        cacheManager.getCache("itemCache").evict(id);
+        return ResponseEntity.ok().build();
     }
 
 }
